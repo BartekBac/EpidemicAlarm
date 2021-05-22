7d8472962c1e4b4f1d346423385b445c567da9bd
DESKTOP-D351Q1F\Bartek <bac2@vp.pl>
2021-05-22 14:07:41 +0200

    initial distance module architecture

---
+++ api/src/main/java/com/epidemicalarm/api/service/distance/DistanceService.java
@@ -0,0 +1,10 @@
+package com.epidemicalarm.api.service.distance;
+
+import com.epidemicalarm.api.service.distance.interfaces.IDistanceService;
+
+public class DistanceService implements IDistanceService {
+    @Override
+    public double calculate(double fromLat, double fromLng, double toLat, double toLng) {
+        return 0;
+    }
+}
