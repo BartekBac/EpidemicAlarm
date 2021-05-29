584a80af7f69e278fd7fdb5144f4a413feacf7eb
DESKTOP-D351Q1F\Bartek <bac2@vp.pl>
2021-05-29 13:17:17 +0200

    added geolocation service

--- ui/epidemic_alarm/lib/src/views/MapWidget.dart
+++ ui/epidemic_alarm/lib/src/views/MapWidget.dart
@@ -1,5 +1,7 @@
+import 'package:epidemic_alarm/src/logic/GeolocationService.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong/latlong.dart';
 
 class MapWidget extends StatefulWidget {
@@ -11,12 +13,46 @@ class MapWidget extends StatefulWidget {
 
 class _MapWidgetState extends State<MapWidget> {
 
+  double _zoom = 5.0;
+  Position _currentPosition = Position(
+      latitude: 51.5,
+      longitude: -0.09,
+      timestamp: DateTime.now(),
+      accuracy: 1.0,
+      altitude: 0.0,
+      heading: 0.0,
+      speed: 0.0,
+      speedAccuracy: 0.0
+  );
+
+  MapOptions _mapOptions;
+
+  void _setCurrentPosition() async {
+    var position = _currentPosition;
+    await GeolocationService.getCurrentPosition()
+        .then((value) => position = value)
+        .catchError((e) {
+      print("Got error: ${e}");
+      return _currentPosition;
+    });
+    setState(() {
+      _currentPosition = position;
+      print("Position set: ${position.toString()}");
+    });
+  }
+
+  @override
+  void initState() {
+    _setCurrentPosition();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
       options: MapOptions(
-        center: LatLng(51.5, -0.09),
-        zoom: 13.0,
+        center: LatLng(_currentPosition.latitude, _currentPosition.longitude),
+        zoom: 5.0,
       ),
       layers: [
         MarkerLayerOptions(
