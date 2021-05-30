da129d61cb2340c21ac2ce6f05c94217944f207f
DESKTOP-D351Q1F\Bartek <bac2@vp.pl>
2021-05-30 13:51:03 +0200

    added map stateful controller and circle marker

---
+++ ui/epidemic_alarm/lib/src/utility/Constants.dart
@@ -0,0 +1,17 @@
+import 'package:geolocator/geolocator.dart';
+
+class Constants {
+
+  static Position defaultPosition = Position(
+      latitude: 50.292222,
+      longitude: 18.6675,
+      timestamp: DateTime.now(),
+      accuracy: 1.0,
+      altitude: 0.0,
+      heading: 0.0,
+      speed: 0.0,
+      speedAccuracy: 0.0
+  );
+
+
+}
--- ui/epidemic_alarm/lib/src/views/MapWidget.dart
+++ ui/epidemic_alarm/lib/src/views/MapWidget.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:epidemic_alarm/src/logic/GeolocationService.dart';
+import 'package:epidemic_alarm/src/utility/Constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong/latlong.dart';
+import 'package:map_controller/map_controller.dart';
 
 class MapWidget extends StatefulWidget {
   MapWidget({Key key}) : super(key: key);
@@ -12,82 +16,81 @@ class MapWidget extends StatefulWidget {
 }
 
 class _MapWidgetState extends State<MapWidget> {
+  MapController mapController;
+  StatefulMapController statefulMapController;
+  StreamSubscription<StatefulMapControllerStateChange> sub;
 
-  double _zoom = 5.0;
-  Position _currentPosition = Position(
-      latitude: 51.5,
-      longitude: -0.09,
-      timestamp: DateTime.now(),
-      accuracy: 1.0,
-      altitude: 0.0,
-      heading: 0.0,
-      speed: 0.0,
-      speedAccuracy: 0.0
-  );
+  Position _position = Constants.defaultPosition;
+  double _zoom = 11.0;
 
-  MapOptions _mapOptions;
+  var circleMarkers = <CircleMarker>[
+    CircleMarker(
+        point: LatLng(37.4219983, -122.084),
+        color: Colors.blue.withOpacity(0.7),
+        borderStrokeWidth: 2,
+        useRadiusInMeter: true,
+        radius: 2000 // 2000 meters | 2 km
+    ),
+  ];
 
-  void _setCurrentPosition() async {
-    var position = _currentPosition;
+  void _setCurrentUserPosition() async {
+    var position = _position;
     await GeolocationService.getCurrentPosition()
         .then((value) => position = value)
         .catchError((e) {
       print("Got error: ${e}");
-      return _currentPosition;
+      return position;
     });
     setState(() {
-      _currentPosition = position;
+      _position = position;
       print("Position set: ${position.toString()}");
     });
+    await statefulMapController.centerOnPoint(LatLng(_position.latitude, _position.longitude));
   }
 
   @override
   void initState() {
-    _setCurrentPosition();
+    // intialize the controllers
+    mapController = MapController();
+    statefulMapController = StatefulMapController(mapController: mapController);
+
+    // wait for the controller to be ready before using it
+    statefulMapController.onReady.then((_) {
+      print("The map controller is ready");
+      _setCurrentUserPosition();
+    });
+
+    /// [Important] listen to the changefeed to rebuild the map on changes:
+    /// this will rebuild the map when for example addMarker or any method
+    /// that mutates the map assets is called
+    sub = statefulMapController.changeFeed.listen((change) => setState(() {}));
     super.initState();
-  }
+    }
 
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-        center: LatLng(_currentPosition.latitude, _currentPosition.longitude),
-        zoom: 5.0,
-      ),
-      layers: [
-        MarkerLayerOptions(
-          markers: [
-            Marker(
-              width: 80.0,
-              height: 80.0,
-              point: LatLng(51.5, -0.09),
-              builder: (ctx) =>
-                  Container(
-                    child: FlutterLogo(),
-                  ),
+    return Scaffold(
+      body: SafeArea(
+          child: Stack(children: <Widget>[
+            FlutterMap(
+              mapController: mapController,
+              options: MapOptions(center: LatLng(_position.latitude, _position.longitude), zoom: _zoom),
+              layers: [
+                statefulMapController.tileLayer,
+                MarkerLayerOptions(markers: statefulMapController.markers),
+                PolylineLayerOptions(polylines: statefulMapController.lines),
+                PolygonLayerOptions(polygons: statefulMapController.polygons),
+                CircleLayerOptions(circles: circleMarkers)
+              ],
             ),
-          ],
-        ),
-      ],
-      children: <Widget>[
-        TileLayerWidget(options: TileLayerOptions(
-            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            subdomains: ['a', 'b', 'c']
-        )),
-        MarkerLayerWidget(options: MarkerLayerOptions(
-          markers: [
-            Marker(
-              width: 80.0,
-              height: 80.0,
-              point: LatLng(51.5, -0.09),
-              builder: (ctx) =>
-                  Container(
-                    child: FlutterLogo(),
-                  ),
-            ),
-          ],
-        )),
-      ],
+            // ...
+          ])),
     );
+  }
+
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
   }
 }
