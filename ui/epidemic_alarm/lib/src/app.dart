1d350fda6ac4f4518a0478ac0ec0331bdc5cb8a3
DESKTOP-D351Q1F\Bartek <bac2@vp.pl>
2021-06-27 14:27:54 +0200

    added working switching top bar menu

--- ui/epidemic_alarm/lib/src/app.dart
+++ ui/epidemic_alarm/lib/src/app.dart
@@ -1,30 +1,15 @@
-import 'package:epidemic_alarm/src/feature/main/ui/main_page.dart';
-import 'package:epidemic_alarm/src/feature/map/fences/model/fences_model.dart';
+import 'package:epidemic_alarm/src/feature/main/ui/menu-widget.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'feature/map/zone/model/zone_model.dart';
 
 class App extends StatelessWidget {
   static const String _title = 'EpidemicAlarm';
 
   @override
   Widget build(BuildContext context) {
-    FencesModel fencesModel = FencesModel();
-    fencesModel.init();
     return MaterialApp(
       title: _title,
       theme: ThemeData.light(),
-      routes: <String, WidgetBuilder>{
-        '/': (context) => ChangeNotifierProvider(
-            create: (context) => fencesModel,
-            child: MainPage()
-        ),
-        '/xd': (context) => ChangeNotifierProvider(
-          create: (context) => ZoneModel(),
-          child: MainPage()
-        )
-      },
+      home: MenuWidget(),
     );
   }
 }
--- ui/epidemic_alarm/lib/src/feature/main/ui/menu-widget.dart
+++ ui/epidemic_alarm/lib/src/feature/main/ui/menu-widget.dart
@@ -1,4 +1,9 @@
+import 'package:epidemic_alarm/src/feature/map/fences/model/fences_model.dart';
+import 'package:epidemic_alarm/src/feature/map/fences/ui/fences_map_widget.dart';
+import 'package:epidemic_alarm/src/feature/map/zone/model/zone_model.dart';
+import 'package:epidemic_alarm/src/feature/map/zone/ui/zone_map_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MenuWidget extends StatefulWidget {
   const MenuWidget({Key key}) : super(key: key);
@@ -8,16 +13,58 @@ class MenuWidget extends StatefulWidget {
 }
 
 class _MenuWidgetState extends State<MenuWidget> with TickerProviderStateMixin {
+  static final String _APP_BAR_TITE = "Epidemic Alarm";
   TabController _tabController;
+  FencesModel _fencesModel;
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 3, vsync: this);
+    _fencesModel = FencesModel();
+    _fencesModel.init();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_APP_BAR_TITE),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const <Widget>[
+            Tab(
+              icon: Icon(Icons.cloud_outlined),
+            ),
+            Tab(
+              icon: Icon(Icons.beach_access_sharp),
+            ),
+            Tab(
+              icon: Icon(Icons.brightness_5_sharp),
+            ),
+          ],
+        ),
+      ),
+      body: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => ZoneModel()),
+          ChangeNotifierProvider(create: (context) => _fencesModel)],
+        child: TabBarView(
+          physics: NeverScrollableScrollPhysics(),
+          controller: _tabController,
+          children: <Widget>[
+            Center(
+              child: ZoneMapWidget()
+            ),
+            Center(
+              child: FencesMapWidget()
+            ),
+            Center(
+              child: Text("It's sunny here"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
